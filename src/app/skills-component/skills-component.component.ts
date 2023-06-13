c03354616031c02f78b8bc1bf95be9eb30fe8b73
Hnaro <arhonprincipe16@gmail.com>
2023-06-13 13:27:13 -0600

    commit

--- src/app/skills-component/skills-component.component.ts
+++ src/app/skills-component/skills-component.component.ts
@@ -6,26 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills-component.component.css']
 })
 export class SkillsComponentComponent {
-  skillLists = [[{ name: "Express", path: "/../../assets/img/expressJS.png"}, {name: "Node", path: "/../../assets/img/nodejs.png" }], [{name:"Angular", path:"/../../assets/img/angular.png"}, {name:"Javascript", path:"/../../assets/img/javascript.png"}], [{name: "MongoDB", path:"/../../assets/img/mongodb.png"}, {name:"Amazon",path:"/../../assets/img/aws.png"}], [{name:"Photoshop",path:"/../../assets/img/phosothop.png"}, {name:"HTML",path:"/../../assets/img/html.png"}],
-                [{name:"CSS", path:"/../../assets/img/css.png"}, {name:"C#", path:"/../../assets/img/cSharp.png"}],[{name:"JQuery", path:"/../../assets/img/jquery.png"},{name:"GitHub", path:"/../../assets/img/github.png"}],[{name:"Typescript", path:"/../../assets/img/typescript.png"}, {name:"Java",path:"/../../assets/img/java.png"}],[{name:"Git",path:"/../../assets/img/git.png"},{name:"New Skills?",path:"/../../assets/img/vacant.png"}]];
+  skillLists = [
+    [
+      { name: "Express", path: "assets/img/expressJS.png"},
+      {name: "Node", path: "assets/img/nodejs.png" }
+    ], [
+      {name:"Angular", path:"assets/img/angular.png"},
+      {name:"Javascript", path:"assets/img/javascript.png"}
+    ], [
+      {name: "MongoDB", path:"assets/img/mongodb.png"},
+      {name:"Amazon",path:"assets/img/aws.png"}
+    ], [
+      {name:"Photoshop",path:"assets/img/phosothop.png"},
+      {name:"HTML",path:"assets/img/html.png"}
+    ], [
+      {name:"CSS", path:"assets/img/css.png"},
+      {name:"C#", path:"assets/img/cSharp.png"}
+    ],[
+      {name:"JQuery", path:"assets/img/jquery.png"},
+      {name:"GitHub", path:"assets/img/github.png"}
+    ],[
+      {name:"Typescript", path:"assets/img/typescript.png"},
+      {name:"Java",path:"assets/img/java.png"}
+    ],[
+      {name:"Git",path:"assets/img/git.png"},
+      {name:"New Skills?",path:"assets/img/vacant.png"}
+    ]
+  ];
 
   skillListDesktop =
   [
-    { name: "Express", path: "/../../assets/img/expressJS.png"},
-    {name: "Node", path: "/../../assets/img/nodejs.png" },
-    {name:"Angular", path:"/../../assets/img/angular.png"},
-    {name:"Javascript", path:"/../../assets/img/javascript.png"},
-    {name: "MongoDB", path:"/../../assets/img/mongodb.png"},
-    {name:"Amazon",path:"/../../assets/img/aws.png"},
-    {name:"Photoshop",path:"/../../assets/img/phosothop.png"},
-    {name:"HTML",path:"/../../assets/img/html.png"},
-    {name:"CSS", path:"/../../assets/img/css.png"},
-    {name:"C#", path:"/../../assets/img/cSharp.png"},
-    {name:"JQuery", path:"/../../assets/img/jquery.png"},
-    {name:"GitHub", path:"/../../assets/img/github.png"},
-    {name:"Typescript", path:"/../../assets/img/typescript.png"},
-    {name:"Java",path:"/../../assets/img/java.png"},
-    {name:"Git",path:"/../../assets/img/git.png"},
-    {name:"New Skills?",path:"/../../assets/img/vacant.png"}
+    { name: "Express", path: "assets/img/expressJS.png"},
+    {name: "Node", path: "assets/img/nodejs.png" },
+    {name:"Angular", path:"assets/img/angular.png"},
+    {name:"Javascript", path:"assets/img/javascript.png"},
+    {name: "MongoDB", path:"assets/img/mongodb.png"},
+    {name:"Amazon",path:"assets/img/aws.png"},
+    {name:"Photoshop",path:"assets/img/phosothop.png"},
+    {name:"HTML",path:"assets/img/html.png"},
+    {name:"CSS", path:"assets/img/css.png"},
+    {name:"C#", path:"assets/img/cSharp.png"},
+    {name:"JQuery", path:"assets/img/jquery.png"},
+    {name:"GitHub", path:"assets/img/github.png"},
+    {name:"Typescript", path:"assets/img/typescript.png"},
+    {name:"Java",path:"assets/img/java.png"},
+    {name:"Git",path:"assets/img/git.png"},
+    {name:"New Skills?",path:"assets/img/vacant.png"}
   ];
 }
