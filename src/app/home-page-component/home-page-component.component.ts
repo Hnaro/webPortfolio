e771855e5bdb158bd43d4617b294c474b5ab0f25
Hnaro <arhonprincipe16@gmail.com>
2023-06-13 17:39:10 -0600

    commit

--- src/app/home-page-component/home-page-component.component.ts
+++ src/app/home-page-component/home-page-component.component.ts
@@ -6,7 +6,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./home-page-component.component.css']
 })
 export class HomePageComponentComponent {
-  @Output updateVisibility = new EventEmitter();
+  @Output() updateVisibility = new EventEmitter();
 
   aboutmeBtn(): void {
     this.updateVisibility.emit({ showHome: true, showSkills: true, showEduAndExp: true, showAbout: false});
