a8c5c2081bbd7be806fb62ab517e6cd079a7d4b0
Hnaro <arhonprincipe16@gmail.com>
2023-06-24 17:50:46 -0600

    Main, skills&tools, education btn updated working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'webportfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('webportfolio');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,84 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webportfolio';
+export class AppComponent implements OnInit {
   isVisible:boolean | undefined;
   messageFromParent:string="";
 
-  homepgVisibility: boolean = false;
-  skillspgVisibility: boolean = true;
-  educationAndExpPageVisibility: boolean = true;
-  aboutPageVisibility: boolean = true;
-
-  isHomeActive: boolean = true;
+  isSkillsSectionActive: boolean = true;
+  isEduSectionActive: boolean = true;
+  isMainSectionActive: boolean = true;
 
   activateStyleAbout: boolean = false;
+
+  isMainActive: boolean = true;
+  isSkillsActive: boolean = false;
+  isEduActive: boolean = false;
+
+  ngOnInit(): void {
+    this.isMainSectionActive = false;
+  }
+
+  onClick(isActive: string) {
+    this.checkActiveBtn(isActive);
+    switch(isActive) {
+      case 'main':
+        this.isMainActive = true;
+        this.isMainSectionActive = !this.isMainActive;
+        break;
+      case 'skills':
+        this.isSkillsActive = true;
+        this.isSkillsSectionActive = !this.isSkillsActive
+        break;
+      case 'edu':
+        this.isEduActive = true;
+        this.isEduSectionActive = !this.isEduActive
+        break;
+    }
+  }
+
+  private checkActiveBtn(isActive: string) {
+    if (isActive.localeCompare('main') == 0) {
+      // turn off active state for other button
+      this.isEduActive = false;
+      this.isSkillsActive = false;
+      // hide
+      this.isEduSectionActive = !this.isEduActive;
+      this.isSkillsSectionActive = !this.isSkillsActive;
+    } else if (isActive.localeCompare('skills') == 0) {
+      this.isMainActive = false;
+      this.isEduActive = false;
+      //hide
+      this.isMainSectionActive = !this.isMainActive;
+      this.isEduSectionActive = !this.isEduActive;
+    } else {
+      this.isMainActive = false;
+      this.isSkillsActive = false;
+      //hide
+      this.isMainSectionActive = !this.isMainActive;
+      this.isSkillsSectionActive = !this.isSkillsActive;
+    }
+  }
 
   aboutBtn(e: any) {
     this.activateStyleAbout = e;
   }
 
-  showHome(e:any) {
-    // opens make the visibility for home to false
-    //this.homepgVisibility = e;
-
-    if (!e.showHome) {
-      this.isHomeActive = true;
-    }else {
-      this.isHomeActive = false;
-    }
-
-    if (!e.showHome) {
-      this.homepgVisibility = e.showHome;
-      this.aboutPageVisibility = true;
-      this.skillspgVisibility = true;
-      this.educationAndExpPageVisibility = true;
-    }
-
-    if (!e.showEduAndExp) {
-      this.homepgVisibility = true;
-      this.aboutPageVisibility = true;
-      this.skillspgVisibility = true;
-      this.educationAndExpPageVisibility = e.showEduAndExp;
-      this.isHomeActive = false;
-    }
-
-    if (!e.showSkills){
-      this.homepgVisibility = true;
-      this.aboutPageVisibility = true;
-      this.skillspgVisibility = e.showSkills;
-      this.educationAndExpPageVisibility = true;
-      this.isHomeActive = false;
-    }
-
-    if (!e.showAbout){
-      this.homepgVisibility = true;
-      this.aboutPageVisibility = e.showAbout;
-      this.skillspgVisibility = true;
-      this.educationAndExpPageVisibility = true;
-      this.isHomeActive = false;
-    }
-  }
-
   recieveUpdatedData(e:any) {
     this.isVisible = e;
-    console.log("receieve from child");
   }
 
   handlevisibility(e:any) {
     this.isVisible = e;
-    console.log("recieved from child: "+e)
   }
 
   sideBar() {
     // send false to child to open the sidebar
     this.isVisible = false;
     //this.messageFromParent = "open";
-    console.log("Parent message to child: "+this.messageFromParent);
   }
 }
