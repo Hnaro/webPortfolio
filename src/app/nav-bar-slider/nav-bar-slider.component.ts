41f0c9d9b425af79a190db538896d1b6589ec164
Hnaro <arhonprincipe16@gmail.com>
2023-06-23 22:48:05 -0600

    css and html style updated

--- src/app/nav-bar-slider/nav-bar-slider.component.ts
+++ src/app/nav-bar-slider/nav-bar-slider.component.ts
@@ -1,71 +1,34 @@
-import { Component, Input, Output, SimpleChanges, OnInit, OnChanges, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar-slider',
   templateUrl: './nav-bar-slider.component.html',
   styleUrls: ['./nav-bar-slider.component.css']
 })
-export class NavBarSliderComponent implements OnInit, OnChanges  {
+export class NavBarSliderComponent {
   @Input() msgFromParent:string = "";
   @Input() visibility:boolean | undefined;
 
-  isHomeActive: boolean = true;
-  isSkillsActive: boolean = false;
+  isHomePageBtnStyleActive: boolean = true;
+  isProjectpageBtnStyleActive: boolean = false;
   isEduAndExpActive: boolean = false;
   @Input() isAboutActive: boolean = false;
 
   @Output() updateDataToParent = new EventEmitter();
   @Output() updateHomeVisibility = new EventEmitter();
 
-  ngOnChanges(changes: SimpleChanges): void {
-    //console.log("current value: "+changes["msgFromParent"].currentValue);
-
-    //if (changes["msgFromParent"].currentValue == "open") {
-      //console.log("it is open");
-      //this.visibility = false;
-    //}
-    if (changes["visibility"].currentValue) {
-      this.visibility = changes["visibility"].currentValue;
-      console.log("new value: "+this.visibility);
-    } else {
-      console.log("undefined");
-    }
-  }
-
-  showHomePage() {
+  onClickHomePage() {
     this.updateDataToParent.emit(true);
     this.updateHomeVisibility.emit({ showHome: false, showSkills: true, showEduAndExp: true, showAbout: true});
-    this.isHomeActive = true;
-    this.isSkillsActive = false;
-    this.isEduAndExpActive = false
-    this.isAboutActive = false;
+    this.isHomePageBtnStyleActive = true;
+    this.isProjectpageBtnStyleActive = false;
   }
 
-  showSkillsPage() {
+  onClickProjects() {
     this.updateHomeVisibility.emit({ showHome: true, showSkills: false, showEduAndExp: true, showAbout: true});
     this.updateDataToParent.emit(true);
-    this.isHomeActive = false;
-    this.isSkillsActive = true;
-    this.isEduAndExpActive = false;
-    this.isAboutActive = false;
-  }
-
-  showEduAndExpPage() {
-    this.updateDataToParent.emit(true);
-    this.updateHomeVisibility.emit({ showHome: true, showSkills: true, showEduAndExp: false, showAbout: true});
-    this.isHomeActive = false;
-    this.isSkillsActive = false;
-    this.isEduAndExpActive = true;
-    this.isAboutActive = false;
-  }
-
-  showAboutPage(): void {
-    this.updateDataToParent.emit(true);
-    this.updateHomeVisibility.emit({ showHome: true, showSkills: true, showEduAndExp: true, showAbout: false});
-    this.isHomeActive = false;
-    this.isSkillsActive = false;
-    this.isEduAndExpActive = false;
-    this.isAboutActive = true;
+    this.isHomePageBtnStyleActive = false;
+    this.isProjectpageBtnStyleActive = true;
   }
 
   close() {
